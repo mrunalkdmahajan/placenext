64cd8d98bf082de8b85f8dd81d8ba0686c7d491d
Latish705 <latishadwani705@gmail.com>
2024-08-25 14:47:47 +0530

    fix allowed host

--- apps/document_server/document_server/settings.py
+++ apps/document_server/document_server/settings.py
@@ -21,27 +21,29 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-5-=^c+h0vc2wdqd*)*9^24rubk8#tfhjgnuv(8&g6(+3t3c6)_"
-ALLOWED_HOSTS = ['.onrender.com', 'localhost', 'localhost:3000','placenext-doc-server']
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', 'localhost:3000','placenext-doc-server','127.0.0.1']
 
 
-print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
+
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
--- apps/document_server/document_server/urls.py
+++ apps/document_server/document_server/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from .views import test
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("test/",test),
 ]
