d2fed451b6eb6cdb668b908d414a42a11bc6979d
mrunalkdmahajan <mrunalkdmahajan@gmail.com>
2024-09-24 00:05:17 +0530

    Adding page specific to user profile

--- apps/web/src/app/college/student_list/[student_Id]/page.tsx
+++ apps/web/src/app/college/student_list/[student_Id]/page.tsx
@@ -1,12 +1,26 @@
 "use client";
 
+import { BackendUrl } from "@/utils/constants";
 import { useParams } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios"
+import Cookies from "js-cookies"
 
 export default function StudentById() {
   const { student_Id } = useParams();
+  const [student,setStudent ] = useState({});
   useEffect(() => {
-    // const fetchStudent = async () => {
+    const fetchStudentById = async ()=>{
+      const res = await axios.get(`${BackendUrl}/api/college/get_student/${student_Id}`,{
+        headers:{
+          Authorization: `Bearer ${localStorage.getItem("token")}`
+        }
+      })
+      
+      if(res.data.success){
+        setStudent(res.data.student);
+      }
+    }
   }, []);
   return <div>{student_Id}</div>;
 }
