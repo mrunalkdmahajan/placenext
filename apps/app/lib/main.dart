9e988affae2a74d6cfd82cd47e01661a31404105
Latish705 <latishadwani705@gmail.com>
2024-09-29 22:05:29 +0530

    working on placenext flutter ios app need fixes

--- apps/app/lib/main.dart
+++ apps/app/lib/main.dart
@@ -1,4 +1,5 @@
 import "package:app/src/pages/college/college_dashboard.dart";
+import "package:app/src/pages/student/application_form.dart";
 import "package:app/src/pages/student/student_dashboard_screen.dart";
 import "package:flutter/material.dart";
 
@@ -7,6 +8,7 @@ import "src/pages/student/login_screen.dart";
 import "src/themes/navigation_theme_data.dart";
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(bottomNavigationBarTheme: NavigationThemeData.theme),
       debugShowCheckedModeBanner: false,
-      initialRoute: "/student_login",
+      initialRoute: "/student_application_form",
       routes: {
         "/": (context) => const OnBoardingScreen(),
         // student routes
@@ -26,7 +28,7 @@ class MyApp extends StatelessWidget {
         "/student_register": (context) => const OnBoardingScreen(),
         "/student_dashboard": (context) => const StudentDashboardScreen(),
         "/student_profile": (context) => const OnBoardingScreen(),
-        "/student_application_form": (context) => const OnBoardingScreen(),
+        "/student_application_form": (context) => const ApplicationForm(),
         // college routes
         "/college_login": (context) => const OnBoardingScreen(),
         "/college_register": (context) => const OnBoardingScreen(),
