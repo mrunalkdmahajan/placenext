92a4dddbf1ec4e2cb57fda33f750144a3580eb4c
Latish705 <latishadwani705@gmail.com>
2024-10-04 23:34:59 +0530

    adding protected routes

--- apps/web/src/app/student/applyjob/page.tsx
+++ apps/web/src/app/student/applyjob/page.tsx
@@ -1,22 +1,12 @@
 "use client";
 
 import ApplicationCards from "@/components/own/Dashboard/ApplicationCards";
+import withAuth from "@/config/services/Student_Auth_service";
 import { BackendUrl } from "@/utils/constants";
 import axios from "axios";
 import { useEffect } from "react";
 
-const StudentInfo = [
-  {
-    name: "Mrunal",
-    description: "A student of vesit",
-  },
-  {
-    name: "Verma",
-    description: "A student of vesit",
-  },
-];
-
-export default function MainDashboard() {
+function MainDashboard() {
   useEffect(() => {
     const getAllJobs = async () => {
       const res = await axios.get(`${BackendUrl}/api/student/companies`, {
@@ -34,3 +24,5 @@ export default function MainDashboard() {
     </div>
   );
 }
+
+export default withAuth(MainDashboard);
